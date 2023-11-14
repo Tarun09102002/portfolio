5e4fd236fde25132f95ea40b6600b91260adf200
Tarun <tarun.agrawal@spit.ac.in>
2023-11-14 13:43:26 +0530

    added animations

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,21 +1,76 @@
 import styles from "./HomePage.module.scss";
 import React from "react";
+import { motion } from "framer-motion";
 
 function HomePage() {
 	return (
 		<div className={styles.container}>
 			<div className={styles.left}>
-				<div className={styles.greeting}>
+				<motion.div
+					className={styles.greeting}
+					whileHover={"hover"}
+					transition={{
+						duration: 0.5,
+						ease: "easeInOut",
+					}}
+					variants={{
+						hover: {
+							scale: 1.2,
+							transition: {
+								duration: 0.5,
+								ease: "easeInOut",
+							},
+						},
+					}}
+				>
 					<div className={styles.hello}>Hello, I am</div>
-					<div className={styles.name}>Tarun Agrawal</div>
+					<motion.div
+						className={styles.name}
+						animate={{
+							rotate: 360,
+						}}
+						transition={{
+							duration: 1,
+							ease: "easeInOut",
+						}}
+						// variants={{
+						// 	hover: {
+						// 		scale: 1.1,
+						// 		rotate: 180,
+						// 		transition: {
+						// 			duration: 0.5,
+						// 			ease: "easeInOut",
+						// 		},
+						// 	},
+						// }}
+						// whileHover="hover"
+					>
+						Tarun Agrawal
+					</motion.div>
 					<div className={styles.role}>Full Stack Web Developer</div>
-				</div>
+				</motion.div>
 				<div className={styles.divider}></div>
 			</div>
 			<div className={styles.right}>
-				<div className={styles.image}>
+				<motion.div
+					className={styles.image}
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition={{ duration: 1 }}
+					whileHover={"hover"}
+					variants={{
+						hover: {
+							scale: 1.1,
+							transition: {
+								duration: 1,
+								ease: "easeInOut",
+							},
+							x: 20,
+						},
+					}}
+				>
 					<img src="tarun-photo-3.png" alt="Tarun Agrawal" />
-				</div>
+				</motion.div>
 			</div>
 		</div>
 	);
